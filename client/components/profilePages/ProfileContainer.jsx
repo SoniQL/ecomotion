74598d777fd2f4bad7334c89c8f77b6cb35021b4
Rebecca Schell <rebeccaschell503@gmail.com>
2021-04-29 10:07:11 -0400

    styling changes and added delete button

--- client/components/profilePages/ProfileContainer.jsx
+++ client/components/profilePages/ProfileContainer.jsx
@@ -28,22 +28,23 @@ class ProfileContainer extends Component {
   render() {
     console.log("Rendering profile container...");
     return (
-
       <div className="profileContainer">
         <div className="nav">
           <div className="navLink" id="profPic">
-            <Link to="/profile">My profile</Link>
+            <Link to="/profile">My Profile</Link>
             <div id="picture"></div>
           </div>
           <div className="navLink" id="savedSearch">
             <Link to="/profile">My Trips</Link>
           </div>
           <div className="navLink" id="newSearch">
-            <Link to="/search">New search</Link>
+            <Link to="/search">New Search</Link>
           </div>
-         <div className="navLink" id="account"><Link to='/account'>Account info</Link></div>
+          <div className="navLink" id="account">
+            <Link to="/account">Account Info</Link>
+          </div>
           <div className="navLink" id="logOut">
-            <Link to="/logout">Log out</Link>
+            <Link to="/logout">Log Out</Link>
           </div>
 
           <div className="logoBox">ecomotion</div>
